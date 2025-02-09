e3e18f19738029d1c9adaef0517db41752aad552
Abdelrahman Abdelmougeth <abdelrahmanabdelmougeth@gmail.com>
2025-02-09 15:52:38 +0200

    adding-client-code

---
+++ client/src/app/shared/models/address.ts
@@ -0,0 +1,9 @@
+export interface IAddress {
+    firstName: string;
+    lastName: string;
+    country: string;
+    street: string;
+    city: string;
+    state: string;
+    postalCode: string;
+}
